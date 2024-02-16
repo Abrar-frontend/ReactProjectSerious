a710c76f6d13d8ac7d1df79f7caf00755049fea9
abrar0349 <abrar0349khan@gmail.com>
2024-02-16 17:45:56 +0500

    React Todo list Projects

---
+++ TodoWithContextapi/src/Context/TodoContext.js
@@ -0,0 +1,17 @@
+import { createContext , useContext } from "react";
+
+export const TodoContext = createContext({
+    todos : [{
+        id : 1,
+        todo : 'todo Sms',
+        completed : false
+    }],
+    addTodo : (todo) => {}
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const ContextProvider = TodoContext.Provider
