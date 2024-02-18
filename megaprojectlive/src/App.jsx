66fab689c7f55c72b20f184a1e87c82e907d17e2
abrar0349 <abrar0349khan@gmail.com>
2024-02-18 15:01:14 +0500

    I am started new project which I do live and it's first class

---
+++ megaprojectlive/src/App.jsx
@@ -0,0 +1,17 @@
+import conf from "./conf/conf"
+
+function App() {
+// console.log(import.meta.env.React_APP_APPWIRTE_URL)
+
+console.log(import.meta.env.VITE_APP_APPWIRTE_URL)
+
+  return (
+    <>
+    
+        
+     <h1>hello friends in mega project</h1>
+    </>
+  )
+}
+
+export default App
