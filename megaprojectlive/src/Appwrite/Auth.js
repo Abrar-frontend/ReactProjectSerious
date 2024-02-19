e34bbd1a0aded4afe83b2f38c5c11124d9e9f69f
abrar0349 <abrar0349khan@gmail.com>
2024-02-19 11:48:44 +0500

    I forgot to call sign in function in singup function

--- megaprojectlive/src/Appwrite/Auth.js
+++ megaprojectlive/src/Appwrite/Auth.js
@@ -20,6 +20,7 @@ export class AuthService{
 
           if(userAccount){
             //I am calling another function
+            this.login({email, password})
            
           } else {
             return userAccount
